1cbc1298aec54d1a83826608b53fe4dbaa49eecc
wei756 <wei756fg@gmail.com>
2025-05-29 02:40:13 +0900

    fix: UI 정리

--- src/components/main/ControlSection/ControlSection.tsx
+++ src/components/main/ControlSection/ControlSection.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import { Button } from '@/components/common';
 import { useStreamer } from '@/hooks/useStreamer';
-import { SettingPanel, StreamKeyInput } from '..';
+import { SettingPanel } from '..';
 import { useState } from 'react';
 
 export const ControlSection = () => {
@@ -10,7 +10,7 @@ export const ControlSection = () => {
 
   const isConnected = connStatus.desktop || connStatus.webcam;
   return (
-    <section className="flex flex-col items-stretch gap-4 bg-white rounded-lg w-[423px] p-2">
+    <section className="flex flex-col items-stretch gap-2 bg-white rounded-lg w-[423px] p-2">
       <Button
         className={clsx({ 'bg-blue-500 text-white': isConnected })}
         onClick={isConnected ? disconnect : connect}
@@ -18,7 +18,6 @@ export const ControlSection = () => {
         {isConnected ? '방송 종료' : '방송 시작'}
       </Button>
       <Button onClick={() => setIsOpenedSettings(!isOpenedSettings)}>설정</Button>
-      <StreamKeyInput />
       {isOpenedSettings && <SettingPanel onClose={() => setIsOpenedSettings(false)} />}
     </section>
   );
