77d98a4f269045299ff6e4c968201f2421986c55
wei756 <wei756fg@gmail.com>
2025-06-15 23:19:09 +0900

    fix: 스트림키 입력 ui 제거

--- src/components/main/SettingPanel/components/ConnectionTab/ConnectionTab.tsx
+++ src/components/main/SettingPanel/components/ConnectionTab/ConnectionTab.tsx
@@ -1,14 +1,9 @@
-import { InputForm } from '@/components/common';
-import { StreamKeyInput } from '@/components/main';
 import { LoginArea } from './components';
 
 export const ConnectionTab = () => {
   return (
     <>
       <LoginArea />
-      <InputForm label="스트림 키">
-        <StreamKeyInput />
-      </InputForm>
     </>
   );
 };
