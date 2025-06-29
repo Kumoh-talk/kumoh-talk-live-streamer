41113b090da6cae612f94434291d220f8dce0156
wei756 <wei756fg@gmail.com>
2025-06-30 05:24:52 +0900

    feat: 방송 시작 전 제목 설정 강제

--- src/components/main/ControlSection/ControlSection.tsx
+++ src/components/main/ControlSection/ControlSection.tsx
@@ -3,18 +3,27 @@ import { Button } from '@/components/common';
 import { SettingPanel } from '..';
 import { useState } from 'react';
 import { useStreamActions, useStreamValue } from '@/context/context';
+import { toast } from 'react-toastify';
 
 export const ControlSection = () => {
   const { connect, disconnect } = useStreamActions();
-  const { connStatus, streamKey } = useStreamValue();
+  const { connStatus, streamKey, title } = useStreamValue();
   const [isOpenedSettings, setIsOpenedSettings] = useState(false);
+
+  const onStartBroadcast = () => {
+    if (title.length === 0) {
+      toast.warn('방송 제목을 먼저 설정해주세요.');
+      return;
+    }
+    connect();
+  };
 
   const isConnected = connStatus.desktop || connStatus.webcam;
   return (
     <section className="flex flex-col items-stretch gap-2 bg-white rounded-lg w-[423px] p-2">
       <Button
         className={clsx({ 'bg-blue-500 text-white': isConnected })}
-        onClick={isConnected ? disconnect : connect}
+        onClick={isConnected ? disconnect : onStartBroadcast}
         disabled={!streamKey || streamKey.length === 0}
       >
         {isConnected ? '방송 종료' : '방송 시작'}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CookiesProvider } from 'react-cookie';
+import { ToastContainer } from 'react-toastify';
 import App from './App.tsx';
 import './index.css';
-import { CookiesProvider } from 'react-cookie';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CookiesProvider>
       <App />
+      <ToastContainer
+        position="bottom-left"
+        limit={5}
+        closeButton={false}
+        autoClose={3000}
+        hideProgressBar
+      />
     </CookiesProvider>
   </React.StrictMode>
 );
